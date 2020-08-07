a61078a2418814c1b5a1379709f222f73840dd3a
Karol Krizka <kkrizka@gmail.com>
2020-08-07 08:35:43 -0700

    Add support for build-time dependencies.

--- pypi2bb.py
+++ pypi2bb.py
@@ -11,6 +11,23 @@ import parsesetup
 import sys, os
 import re
 
+def sanitize_name(name):
+    """
+    Sanitizes the package name to something that can be used
+    by Yocto.
+
+    The steps are:
+     - lower case
+     - replace _ with -
+     - remove version requirements
+    """
+    re_ver=re.compile('[=><]')
+    name=name.lower()
+    name=name.replace('_','-')
+    name=re_ver.split(name)[0]
+    return name
+
+
 #
 # Arguments to CLI
 parser = argparse.ArgumentParser(description='Generate Yocto recipes from PyPi packages')
@@ -42,6 +59,7 @@ pname=d['info']['name'] if d['info']['name']!=package else None
 licname=None
 licmd5=None
 depends=[]
+rdepends=[]
 
 with tempfile.TemporaryDirectory() as tmpdirname:
     tarr=requests.get(v['url'])
@@ -83,12 +101,11 @@ with tempfile.TemporaryDirectory() as tmpdirname:
     setup_args = parsesetup.parse_setup(tmpdirname+'/'+pdirname+'/setup.py', trusted='True')
     os.chdir(cwd)
 
-    re_ver=re.compile('[=><]')
+    for pkg in setup_args.get('setup_requires',[]):
+        depends.append(sanitize_name(pkg))
+
     for pkg in setup_args.get('install_requires',[]):
-        pkg=pkg.lower()
-        pkg=pkg.replace('_','-')
-        pkg=re_ver.split(pkg)[0]
-        depends.append(pkg)
+        rdepends.append(sanitize_name(pkg))
 
 #
 # Create the files
@@ -96,8 +113,9 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 # inc file
 t_inc=env.get_template('python.inc')
-incfile=t_inc.render(homepage=d['info']['home_page'], summary=d['info']['summary'], license=license, licname=licname, licmd5=licmd5, realname=pname, pkgext=ext,
-                         md5sum=v['digests']['md5'],sha256sum=v['digests']['sha256'],depends=depends)
+incfile=t_inc.render(homepage=d['info']['home_page'], summary=d['info']['summary'], license=license, licname=licname, licmd5=licmd5,
+                         realname=pname, pkgext=ext,md5sum=v['digests']['md5'],sha256sum=v['digests']['sha256'],
+                         rdepends=rdepends, depends=depends)
 f=open('python-{}.inc'.format(package),'w')
 f.write(incfile)
 f.close()
